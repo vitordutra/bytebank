b40a221edbb4aedbb248171d254b74ccf1b96392
Vitor Dutra <jo.vitor.d@gmail.com>
2021-12-30 21:14:02 -0300

    04-Implementando o design do item de transferência

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white70,
+      ),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000-0'),
+              ),
             ),
-          ])
-        ],
-      ), //Column
+            Card(
+              child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('200.0'),
+                  subtitle: Text('1000-0')),
+            ),
+            Card(
+              child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('300.0'),
+                  subtitle: Text('1000-0')),
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text("Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
     );
+  }
+}
