57df4ca91ba296220caef59df4b84771b786f3f5
Vitor Dutra <jo.vitor.d@gmail.com>
2022-01-03 15:55:21 -0300

    Ajustando o Tema do Projeto

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,16 @@ class ByteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white70,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary),
       ),
-      home: Scaffold(
-        body: ListaTransferencias(),
-      ),
+      home: ListaTransferencias(),
     );
   }
 }
